e0c69bc042da9da2dd98779f720f78740fd1040d
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-10-07 21:56:50 -0300

    fix(order-service): create a service.

---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/OrderService.java
@@ -0,0 +1,12 @@
+package br.com.microservices.orchestrated.orderservice.core.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class OrderService {
+
+    
+
+}
