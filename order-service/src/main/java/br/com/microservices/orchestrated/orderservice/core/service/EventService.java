175f3e873f51fda6216b4e9e61ec3edce5d667f6
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-10-07 22:11:00 -0300

    fix(order-service): create a notify ending step request.

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
@@ -3,14 +3,26 @@ package br.com.microservices.orchestrated.orderservice.core.service;
 import br.com.microservices.orchestrated.orderservice.core.document.Event;
 import br.com.microservices.orchestrated.orderservice.core.repository.EventRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
+@Slf4j
 @Service
 @AllArgsConstructor
 public class EventService {
     private EventRepository repository;
 
-    public Event save(Event event){
+    public Event save(Event event) {
         return repository.save(event);
+    }
+
+    public void notifyEnding(Event event) {
+        event.setOrderId(event.getOrderId());
+        event.setCreatedAt(LocalDateTime.now());
+        save(event);
+
+        log.info("Order {} with saga notified! TransactionalId {}", event.getOrderId(), event.getTransactionId());
     }
 }
