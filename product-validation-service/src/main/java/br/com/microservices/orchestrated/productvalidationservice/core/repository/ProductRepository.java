9b0e68bf69f9895ba41d228f576a1f764fb29a5d
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-10-10 20:43:57 -0300

    fix(product-validation-service): create a repositories product and validation.

---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package br.com.microservices.orchestrated.productvalidationservice.core.repository;
+
+import br.com.microservices.orchestrated.productvalidationservice.core.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/repository/ValidationRepository.java
@@ -0,0 +1,7 @@
+package br.com.microservices.orchestrated.productvalidationservice.core.repository;
+
+import br.com.microservices.orchestrated.productvalidationservice.core.model.Validation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ValidationRepository extends JpaRepository<Validation, Integer> {
+}
